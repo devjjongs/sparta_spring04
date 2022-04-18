c3239444f5caa3153a45e837549749ee1fdc7281
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-04-18 23:59:57 +0900

    added source codes
    키워드를 이용한 상품 검색 및 출력을 위한 Jquwey ajax 코드 작성

--- src/main/resources/static/basic.js
+++ src/main/resources/static/basic.js
@@ -47,10 +47,29 @@ function execSearch() {
      * 검색결과 목록: #search-result-box
      * 검색결과 HTML 만드는 함수: addHTML
      */
-    // 1. 검색창의 입력값을 가져온다.
+        // 1. 검색창의 입력값을 가져온다.
+    let query = $('#query').val();
+
     // 2. 검색창 입력값을 검사하고, 입력하지 않았을 경우 focus.
+    if (query == '') {
+        alert('검색어를 입력해주세요');
+        $('#query').focus();
+        return;
+    }
     // 3. GET /api/search?query=${query} 요청
-    // 4. for 문마다 itemDto를 꺼내서 HTML 만들고 검색결과 목록에 붙이기!
+    $.ajax({
+        type: 'GET',
+        url: `/api/search?query=${query}`,
+        success: function (response) {
+            $('#search-result-box').empty();
+            // 4. for 문마다 itemDto를 꺼내서 HTML 만들고 검색결과 목록에 붙이기!
+            for (let i = 0; i < response.length; i++) {
+                let itemDto = response[i];
+                let tempHtml = addHTML(itemDto);
+                $('#search-result-box').append(tempHtml);
+            }
+        }
+    })
 
 }
 
@@ -60,7 +79,21 @@ function addHTML(itemDto) {
      * image, title, lprice, addProduct 활용하기
      * 참고) onclick='addProduct(${JSON.stringify(itemDto)})'
      */
-    return ``
+    return `<div class="search-itemDto">
+                <div class="search-itemDto-left">
+                    <img src="${itemDto.image}" alt="">
+                </div>
+                <div class="search-itemDto-center">
+                    <div>${itemDto.title}</div>
+                    <div class="price">
+                        ${numberWithCommas(itemDto.lprice)} // 금액에 , 를 찍어서 반환
+                        <span class="unit">원</span>
+                    </div>
+                </div>
+                <div class="search-itemDto-right">
+                    <img src="images/icon-save.png" alt="" onclick='addProduct(${JSON.stringify(itemDto)})'>
+                </div>
+            </div>`
 }
 
 function addProduct(itemDto) {
@@ -71,6 +104,7 @@ function addProduct(itemDto) {
      * 2. data: JSON.stringify(itemDto),
      */
     // 1. POST /api/products 에 관심 상품 생성 요청
+
     // 2. 응답 함수에서 modal을 뜨게 하고, targetId 를 reponse.id 로 설정 (숙제로 myprice 설정하기 위함)
 }
 
